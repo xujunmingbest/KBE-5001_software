438678185ba78244b62c6a437de141749859c9ae
xjm <925271237@qq.com>
2018-04-24 13:21:50 +0800

    旧的vs工程 拖入windows media player 控件失败是因为 引用有错误，删除原来引用，重新导入引用

--- 电工基础1本/实验1原理说明视频.h
+++ 电工基础1本/实验1原理说明视频.h
@@ -8,7 +8,7 @@ namespace 电工基础1本 {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-	using namespace System::Threading;
+
 	/// <summary>
 	/// 实验1原理说明视频 摘要
 	/// </summary>
@@ -18,41 +18,12 @@ namespace 电工基础1本 {
 		实验1原理说明视频(void)
 		{
 			InitializeComponent();
-			CheckForIllegalCrossThreadCalls = false;
-			//axWindowsMediaPlayer1->URL = "E:\\clr\\电工基础1本\\资源\\二极管伏安特性曲线测试实验.mp4";
 			实验1原理说明视频IsOpened = true;
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->Font = gcnew System::Drawing::Font("Microsoft Sans Serif", 13, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Pixel, ((byte)(134)));
-			t = gcnew Thread(gcnew ThreadStart(this,&实验1原理说明视频::test));
-			t->IsBackground = true;
-			t->Start();
 			//
 			//TODO:  在此处添加构造函数代码
 			//
 		}
-		Thread ^ t;
-		void test() {
-			while (1) {
-				try {
-					int a = axWindowsMediaPlayer1->Ctlcontrols->currentPosition;
-					switch (a) {
-					case 1:Console::Write(a.ToString() + "显示"); break;
-					case 2:Console::Write(a.ToString() + "显示"); break;
-					case 3:Console::Write(a.ToString() + "显示"); break;
-					case 4:Console::Write(a.ToString() + "显示"); break;
-					case 5:Console::Write(a.ToString() + "显示"); break;
-					case 6:Console::Write(a.ToString() + "显示"); break;
-					}
 
-					cout << axWindowsMediaPlayer1->Ctlcontrols->currentPosition << endl;
-				}
-				catch (System::Exception ^e) {
-					Console::Write(e->ToString()) ;
-				}
-				Sleep(1000);
-			}
-		
-		}
 	protected:
 		/// <summary>
 		/// 清理所有正在使用的资源。
@@ -60,13 +31,16 @@ namespace 电工基础1本 {
 		~实验1原理说明视频()
 		{
 			实验1原理说明视频IsOpened = false;
-			t->Abort();
 			if (components)
 			{
 				delete components;
 			}
 		}
 	private: AxWMPLib::AxWindowsMediaPlayer^  axWindowsMediaPlayer1;
+	protected:
+
+	protected:
+
 	protected:
 
 	private:
@@ -90,21 +64,19 @@ namespace 电工基础1本 {
 			// axWindowsMediaPlayer1
 			// 
 			this->axWindowsMediaPlayer1->Enabled = true;
-			this->axWindowsMediaPlayer1->Location = System::Drawing::Point(13, 13);
+			this->axWindowsMediaPlayer1->Location = System::Drawing::Point(12, 12);
 			this->axWindowsMediaPlayer1->Name = L"axWindowsMediaPlayer1";
 			this->axWindowsMediaPlayer1->OcxState = (cli::safe_cast<System::Windows::Forms::AxHost::State^>(resources->GetObject(L"axWindowsMediaPlayer1.OcxState")));
-			this->axWindowsMediaPlayer1->Size = System::Drawing::Size(473, 429);
+			this->axWindowsMediaPlayer1->Size = System::Drawing::Size(481, 399);
 			this->axWindowsMediaPlayer1->TabIndex = 0;
-			this->axWindowsMediaPlayer1->Enter += gcnew System::EventHandler(this, &实验1原理说明视频::axWindowsMediaPlayer1_Enter);
 			// 
 			// 实验1原理说明视频
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 15);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(500, 454);
+			this->ClientSize = System::Drawing::Size(505, 423);
 			this->Controls->Add(this->axWindowsMediaPlayer1);
 			this->Name = L"实验1原理说明视频";
-			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"实验1原理说明视频";
 			this->Load += gcnew System::EventHandler(this, &实验1原理说明视频::实验1原理说明视频_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->axWindowsMediaPlayer1))->EndInit();
@@ -113,8 +85,6 @@ namespace 电工基础1本 {
 		}
 #pragma endregion
 	private: System::Void 实验1原理说明视频_Load(System::Object^  sender, System::EventArgs^  e) {
-	}
-	private: System::Void axWindowsMediaPlayer1_Enter(System::Object^  sender, System::EventArgs^  e) {
 	}
 	};
 }
